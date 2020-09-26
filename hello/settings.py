00838678ec7258507269138e57b41490cc3d2c5a
HamidFred <hohrir@gmail.com>
2020-09-26 17:27:03 +0000

    Run CollectStatic

--- hello/settings.py
+++ hello/settings.py
@@ -107,7 +107,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR + '/assets/'
+STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 
 STATICFILES_DIRS = [STATIC_DIR,]
 
